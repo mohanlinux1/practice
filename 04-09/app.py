539bdf02344fcb1a437b86b56707766e3432201d
mohanlinux1 <143994419+mohanlinux1@users.noreply.github.com>
2023-09-11 12:00:20 +0530

    Add files via upload

---
+++ 04-09/app.py
@@ -0,0 +1,14 @@
+name = "Deepak"
+address = "Namakkal"
+total_subjects = 5
+sub1 = 68
+sub2 = 89
+sub3 = 90
+sub4 = 89
+sub5 = 78
+
+total = sub1 + sub2 + sub3 + sub4 + sub5
+
+average = (total / total_subjects)
+
+print ( " i m ", name, "i m from ", address, "My 12th marks total is : ",total , " My average mark is : ", average)
